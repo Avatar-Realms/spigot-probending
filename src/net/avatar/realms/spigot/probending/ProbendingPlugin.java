9487e3964027bb1db450974251436651d1998759
Noko <nokorbis@gmail.com>
2016-04-03 01:47:19 +0200

    Base plugin configuration

--- src/net/avatar/realms/spigot/probending/ProbendingPlugin.java
+++ src/net/avatar/realms/spigot/probending/ProbendingPlugin.java
@@ -1,7 +1,25 @@
 package net.avatar.realms.spigot.probending;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by Nokorbis on 3/04/2016.
  */
-public class ProbendingPlugin {
+public class ProbendingPlugin extends JavaPlugin {
+
+    private static ProbendingPlugin instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+    }
+
+    public static ProbendingPlugin getInstance() {
+        return instance;
+    }
 }
---
+++ src/net/avatar/realms/spigot/probending/models/ProbendingTeam.java
@@ -0,0 +1,9 @@
+package net.avatar.realms.spigot.probending.models;
+
+/**
+ * Created by Nokorbis on 3/04/2016.
+ */
+public class ProbendingTeam {
+
+    private String name;
+}
