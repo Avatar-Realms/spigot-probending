4810e0754ce85657a53853d7adc59280fe0fef99
Noko <nokorbis@gmail.com>
2016-04-06 01:40:35 +0200

    Base of probending tournaments

--- src/net/avatar/realms/spigot/probending/models/ProbendingRound.java
+++ src/net/avatar/realms/spigot/probending/models/ProbendingRound.java
@@ -4,4 +4,11 @@ package net.avatar.realms.spigot.probending.models;
  * Created by Nokorbis on 3/04/2016.
  */
 public class ProbendingRound {
+
+    private ProbendingMatch match;
+    private ProbendingTeam winner;
+
+    public ProbendingRound(ProbendingMatch match) {
+        this.match = match;
+    }
 }
--- src/net/avatar/realms/spigot/probending/models/ProbendingTournament.java
+++ src/net/avatar/realms/spigot/probending/models/ProbendingTournament.java
@@ -1,7 +1,34 @@
 package net.avatar.realms.spigot.probending.models;
 
+import java.util.*;
+
 /**
  * Created by Nokorbis on 3/04/2016.
  */
 public class ProbendingTournament {
+
+    private Set<ProbendingTeam> registeredTeams;
+
+    private Map<TournamentPhase, List<ProbendingMatch>> matches;
+
+    private TournamentPhase currentPhase;
+
+    public ProbendingTournament() {
+        this.currentPhase = TournamentPhase.ELIMINATIONS;
+        this.registeredTeams = new HashSet<ProbendingTeam>();
+        matches = new HashMap<TournamentPhase, List<ProbendingMatch>>();
+        matches.put(TournamentPhase.ELIMINATIONS, new LinkedList<ProbendingMatch>());
+    }
+
+    public void register(ProbendingTeam team) {
+
+    }
+
+    public boolean isRegistered(ProbendingTeam team) {
+        return registeredTeams.contains(team);
+    }
+
+    public void unregister(ProbendingTeam team) {
+        registeredTeams.remove(team);
+    }
 }
