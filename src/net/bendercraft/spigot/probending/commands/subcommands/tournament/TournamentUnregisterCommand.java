52458f84548a4f298ef42692048b87868f0dbf7e
Noko <deboisjerome@gmail.com>
2016-06-06 13:39:20 +0200

    PRobending unregister command

---
+++ src/net/bendercraft/spigot/probending/commands/subcommands/tournament/TournamentUnregisterCommand.java
@@ -0,0 +1,51 @@
+package net.bendercraft.spigot.probending.commands.subcommands.tournament;
+
+import net.bendercraft.spigot.probending.commands.ProbendingCommand;
+import net.bendercraft.spigot.probending.data.Container;
+import net.bendercraft.spigot.probending.exceptions.ProbendingException;
+import net.bendercraft.spigot.probending.exceptions.ProbendingPermissionException;
+import net.bendercraft.spigot.probending.models.ProbendingTeam;
+import net.bendercraft.spigot.probending.models.ProbendingTournament;
+import net.bendercraft.spigot.probending.utils.Messages;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+/**
+ * Created by Nokorbis on 06/06/2016.
+ */
+public class TournamentUnregisterCommand extends ProbendingCommand {
+
+    public TournamentUnregisterCommand() {
+        this.command = "unregister";
+        this.aliases.add("unreg");
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, List<String> args) throws ProbendingException {
+        if (!sender.hasPermission("probending.team.manage")) {
+            throw new ProbendingPermissionException();
+        }
+        if (args.isEmpty()) {
+            throw new ProbendingException("error.command.argument.more");
+        }
+
+        String teamName = args.remove(0);
+        ProbendingTeam team = Container.getInstance().getTeam(teamName);
+        ProbendingTournament tour = ProbendingTournament.getTournament();
+
+        if (!tour.isRegistered(team)) {
+            throw new ProbendingException("error.command.team.notregistered");
+        }
+        tour.unregister(team);
+        String msg = Messages.get("probending.team.unregistered").replace("{TEAM}", team.getName());
+        sender.sendMessage(msg);
+        return true;
+    }
+
+    @Override
+    public void printUsage(CommandSender sender) {
+        sender.sendMessage(ChatColor.AQUA + "/probending unregister <TEAM_NAME>");
+    }
+}
